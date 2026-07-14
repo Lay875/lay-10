fe5d2ebd3e4c9cfa86e14d5dab5e029da59fb111
Lay875 <875204105@qq.com>
2026-07-14 18:14:18 +0800

    Add files via upload

---
+++ public/assets/vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+});
